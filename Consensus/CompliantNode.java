b7286595352e0776c7f48ac0858cdfde7fb8732a
Chi <chi.wang@salesforce.com>
2018-03-05 12:04:20 -0800

    Assignment 4

---
+++ Consensus/CompliantNode.java
@@ -0,0 +1,104 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
+
+
+///* CompliantNode refers to a node that follows the rules (not malicious)*/
+//public class CompliantNode implements Node {
+//
+//    private double p_graph;
+//    private double p_malicious;
+//    private double p_tXDistribution;
+//    private int numRounds;
+//
+//    private boolean[] followees;
+//    private boolean[] blackListed;
+//
+//    private Set<Transaction> pendingTransactions;
+//    private HashSet<Transaction> processedTransactions;
+//
+//    public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
+//        this.p_graph = p_graph;
+//        this.p_malicious = p_malicious;
+//        this.p_tXDistribution = p_txDistribution;
+//        this.numRounds = numRounds;
+//        this.processedTransactions = new HashSet<>();
+//    }
+//
+//    public void setFollowees(boolean[] followees) {
+//        this.followees = followees;
+//        this.blackListed = new boolean[followees.length];
+//    }
+//
+//    public void setPendingTransaction(Set<Transaction> pendingTransactions) {
+//        this.pendingTransactions = pendingTransactions;
+//    }
+//
+//    public Set<Transaction> sendToFollowers() {
+//        Set<Transaction> toSend = new HashSet<>(pendingTransactions);
+//        processedTransactions.addAll(toSend);
+//        pendingTransactions.clear();
+//        return toSend;
+//    }
+//
+//    public void receiveFromFollowees(Set<Candidate> candidates) {
+//        for (Candidate c : candidates) {
+//            if (!processedTransactions.contains(c.tx)) {
+//                pendingTransactions.add(c.tx);
+//            }
+//        }
+//    }
+//}
+
+public class CompliantNode implements Node {
+
+    private double p_graph;
+    private double p_malicious;
+    private double p_tXDistribution;
+    private int numRounds;
+
+    private boolean[] followees;
+
+    private Set<Transaction> pendingTransactions;
+
+    private boolean[] blackListed;
+
+    public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
+        this.p_graph = p_graph;
+        this.p_malicious = p_malicious;
+        this.p_tXDistribution = p_txDistribution;
+        this.numRounds = numRounds;
+    }
+
+    public void setFollowees(boolean[] followees) {
+        this.followees = followees;
+        this.blackListed = new boolean[followees.length];
+    }
+
+    public void setPendingTransaction(Set<Transaction> pendingTransactions) {
+        this.pendingTransactions = pendingTransactions;
+    }
+
+    public Set<Transaction> sendToFollowers() {
+        Set<Transaction> toSend = new HashSet<>(pendingTransactions);
+        pendingTransactions.clear();
+        return toSend;
+    }
+
+    public void receiveFromFollowees(Set<Candidate> candidates) {
+        Set<Integer> senders = candidates.stream().map(c -> c.sender).collect(toSet());
+
+        // Black list malicious nodes.
+        for (int i = 0; i < followees.length; i++) {
+            if (followees[i] && !senders.contains(i))
+                blackListed[i] = true;
+        }
+        for (Candidate c : candidates) {
+            if (!blackListed[c.sender]) {
+                pendingTransactions.add(c.tx);
+            }
+        }
+    }
+}
